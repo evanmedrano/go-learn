db2335439663bc750d8fd19114ea6b8c02aa626d
Evan Medrano <evan.a.medrano@gmail.com>
2020-07-15 14:49:04 -0400

    Add learning-related files

---
+++ firstapp/Main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	isAdmin = 1 << iota
+	isHeadquarters
+	canSeeFinancials
+
+	canSeeAfrica
+	canSeeAsia
+	canSeeEurope
+	canSeeNorthAmerica
+	canSeeSouthAmerica
+)
+
+func main() {
+	var roles byte = isAdmin | canSeeFinancials | canSeeEurope
+	fmt.Printf("%b\n", roles)
+	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
+	fmt.Printf("Is HQ? %v\n", isHeadquarters&roles == isHeadquarters)
+}
